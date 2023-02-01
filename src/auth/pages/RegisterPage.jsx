1819b48c505b694ce175168430b33e1cb3113cea
Luis Carriel <imagetech.developer11@outlook.com>
2023-01-31 23:20:01 -0500

    layout

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,58 @@
 
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <AuthLayout title="Crear cuenta">
+      <form>
+        <Grid container spacing={2}>
+        <Grid item xs={12}>
+            <TextField
+              label="Nombre completo"
+              type="text"
+              placeholder="Luis Carriel"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="contraseña"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} mt={1}>
+          <Grid item xs={12} >
+            <Button variant="contained" fullWidth>
+              {" "}
+              Crear cuenta
+            </Button>
+          </Grid>
+         
+        </Grid>
+
+        <Grid container direction="row" justifyContent="end" mt={1}>
+          <Typography sx={{mr:1}}>¿Ya tienes cuenta?  </Typography>
+          <Link component={RouterLink} color="inhereit" to="/auth/login">
+            Ingresar
+          </Link>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
+    <JournalLayout>
+    <Typography> asdasdadsas</Typography>
  
-    </>
+    </JournalLayout>
   );
 };
