6acb6f5d3f7f677b189d6f9bb58399120975c758
Luis Carriel <imagetech.developer11@outlook.com>
2023-02-02 00:23:41 -0500

    finalizar style

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,10 +1,15 @@
+import { Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
+import { Navbar, SideBar } from "../components";
 
 const drawerWidth = 240;
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
+      <Navbar drawerWidth={drawerWidth} />
+      <SideBar drawerWidth={drawerWidth} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         {children}
       </Box>
     </Box>
